0e8115dd797f1ccc570fef81b9e26e5d0f8c926b
Nizzoli <laurent.nizzoli@thermofisher.com>
2023-05-15 22:57:29 +0200

    get messages chat

--- chatty-backend/src/features/chat/controllers/get-chat-messages.ts
+++ chatty-backend/src/features/chat/controllers/get-chat-messages.ts
@@ -20,4 +20,20 @@ export class Get {
     }
     res.status(HTTP_STATUS.OK).json({ message: 'User conversation list fetched.', list });
   }
+
+  public async messages(req: Request, res: Response): Promise<void> {
+    const { receiverId } = req.params;
+    let messages: IMessageData[] = [];
+    const cachesMessages: IMessageData[] = await messageCache.getChatMessagesFromCache(`${req.currentUser!.userId}`, `${receiverId}`);
+    if (cachesMessages.length) {
+      messages = cachesMessages;
+    } else {
+      messages = await chatService.getMessages(
+        new mongoose.Types.ObjectId(req.currentUser!.userId),
+        new mongoose.Types.ObjectId(receiverId),
+        { createdAt: 1 }
+      );
+    }
+    res.status(HTTP_STATUS.OK).json({ message: 'User chat messages  ', messages });
+  }
 }
--- chatty-backend/src/shared/services/redis/message.cache.ts
+++ chatty-backend/src/shared/services/redis/message.cache.ts
@@ -4,7 +4,7 @@ import { BaseCache } from '@service/redis/base.cache';
 import Logger from 'bunyan';
 import { config } from '@root/config';
 import { ServerError } from '@global/helpers/error-handler';
-import { findIndex } from 'lodash';
+import { find, findIndex } from 'lodash';
 
 const log: Logger = config.createLogger('messageCache');
 
@@ -152,6 +152,32 @@ export class MessageCache extends BaseCache {
         conversationChatList.push(Helpers.parseJson(lastMessage));
       }
       return conversationChatList;
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
+
+  public async getChatMessagesFromCache(senderId: string, receiverId: string): Promise<IMessageData[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const userChatList: string[] = await this.client.LRANGE(`chatList:${senderId}`, 0, -1);
+      // const receiver: string = userChatList.filter((chatItem: string) => chatItem === receiverId)[0];
+      const receiver: string = find(userChatList, (listItem: string) => listItem.includes(receiverId)) as string;
+      const parsedReceiver: IChatList = Helpers.parseJson(receiver) as IChatList;
+      if (parsedReceiver) {
+        const userMessages: string[] = await this.client.LRANGE(`messages:${parsedReceiver.conversationId}`, 0, -1);
+        const chatMessages: IMessageData[] = [];
+        for (const item of userMessages) {
+          const chatItem = Helpers.parseJson(item) as IMessageData;
+          chatMessages.push(chatItem);
+        }
+        return chatMessages;
+      } else {
+        return [];
+      }
     } catch (error) {
       log.error(error);
       throw new ServerError('Server error. Try again.');
