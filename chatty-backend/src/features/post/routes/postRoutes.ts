2f5f235d1400ee36acc13562de1ac93d0d401edb
Nizzoli <laurent.nizzoli@thermofisher.com>
2023-05-30 15:38:29 +0200

    add video to post

--- chatty-backend/src/features/post/routes/postRoutes.ts
+++ chatty-backend/src/features/post/routes/postRoutes.ts
@@ -23,6 +23,8 @@ class PostRoutes {
 
     this.router.put('/post/:postId', authMiddleware.checkAuthentication, Update.prototype.post);
     this.router.put('/post/image/:postId', authMiddleware.checkAuthentication, Update.prototype.postWithImage);
+    this.router.put('/post/video/:postId', authMiddleware.checkAuthentication, Update.prototype.postWithVideo);
+
     this.router.delete('/post/:postId', authMiddleware.checkAuthentication, Delete.prototype.post);
     return this.router;
   }
